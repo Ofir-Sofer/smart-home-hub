b6eb82bf302ce9d17629512fa0ea4513416126ce
Ofir-Sofer <ofirsofer1@gmail.com>
2026-07-13 15:47:13 +0300

    Forward description/commands/values lookups through DeviceRegistry

--- include/device_management/registry/device_registry.hpp
+++ include/device_management/registry/device_registry.hpp
@@ -20,6 +20,9 @@ public:
     // Lifetime is tied to the DeviceRegistry instance.
     [[nodiscard]] MessageQueue<Message>* get_queue(const std::string& device_id);
     [[nodiscard]] std::vector<std::string> get_device_id_list() const;
+    [[nodiscard]] std::string get_description(const std::string& device_id) const;
+    [[nodiscard]] std::vector<std::string> get_commands(const std::string& device_id) const;
+    [[nodiscard]] std::vector<std::string> get_command_values(const std::string& device_id, const std::string& command) const;
     void shutdown_all_queues();
 
 private:
